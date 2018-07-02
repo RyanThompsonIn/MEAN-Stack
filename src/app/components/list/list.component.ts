6e5f2ceed0d030b0a452a968bd4cdd3fa470e78d
IFB\OPR9511 <ryan.thompson@infarmbureau.com>
2018-07-02 10:45:42 -0400

    missed commit

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { IssueService } from '../../services/issue.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  constructor(
+    private issueService: IssueService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/services/issue.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IssueService {
+
+  uri = 'http://localhost:4000';
+
+  constructor(private http: HttpClient) {
+  }
+
+  addIssue(title, responsible, description, severity) {
+    const issue = {
+      title: title,
+      responsible: responsible,
+      description: description,
+      severity: severity
+    };
+    return this.http.put(`${this.uri}/issues`, issue);
+  }
+
+  getIssues() {
+    return this.http.get(`${this.uri}/issues`);
+  }
+
+  getIssueById(id) {
+    return this.http.get(`${this.uri}/issues/${id}`);
+  }
+
+  updateIssue(id, title, responsible, description, severity, status) {
+    const issue = {
+      title: title,
+      responsible: responsible,
+      description: description,
+      severity: severity,
+      status: status
+    };
+    return this.http.post(`${this.uri}/issues/${id}`, issue);
+  }
+
+  deleteIssue(id) {
+    return this.http.delete(`${this.uri}/issues/${id}`);
+  }
+}
